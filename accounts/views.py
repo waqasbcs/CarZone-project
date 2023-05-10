cf0b86f78cc3cc1c69fe6a9189c8c6a63bf00f52
waqasbcs <waqasaliy2945@gmail.com>
2023-05-10 12:35:20 +0500

    logout updated

--- accounts/views.py
+++ accounts/views.py
@@ -62,6 +62,10 @@ def dashboard(request):
     return render(request,'accounts/dashboard.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request,'you have successfully logged out!!!')
+        return redirect('home')
     return redirect('home')
 
 
