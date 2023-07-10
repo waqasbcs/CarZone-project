fbd2532f25fab9456abacfe8b715cf08bd4d141c
waqasbcs <waqasaliy2945@gmail.com>
2023-07-10 11:15:59 +0500

    updated

--- CarZone1/settings.py
+++ CarZone1/settings.py
@@ -165,7 +165,7 @@ SITE_ID = 1
 #setting email
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587            
-EMAIL_HOST_USER = 'waqasaliy2945@gmail.com',
+EMAIL_HOST_USER = 'waqasaliy2945@gmail.com'
 EMAIL_HOST_PASSWORD = 'oxlvonfklhvivegc'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- accounts/views.py
+++ accounts/views.py
@@ -25,38 +25,36 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        firstname = request.POST['firstname']
-        lastname = request.POST['lastname']
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        confirm_password= request.POST['confirm_password']
-         # Check if any of the required fields are empty
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+        
+        # Check if any of the required fields are empty
         if not firstname or not lastname or not email or not password or not confirm_password or not username:
             messages.error(request, 'Please fill all the required fields.')
             return redirect('register')
         
         if password == confirm_password:
             if User.objects.filter(username=username).exists():
-                messages.error(request,'User name already exists')
+                messages.error(request, 'Username already exists.')
                 return redirect('register')
-            else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request,'email already exists')
-                    return redirect('register')
-                else:
-                    user = User.objects.create_user(first_name=firstname, last_name=lastname,username=username,email=email,password=password)
-                    auth.login(request, user)
-                    messages.success(request,'You are now logged in')
-                    return redirect('dashboard')
-                    user.save()
-                    messages.success(request,'you are registered successfully')
-                    return redirect('login')
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists.')
+                return redirect('register')
+            
+            user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+            auth.login(request, user)
+            messages.success(request, 'You are now registered and logged in.')
+            return redirect('dashboard')
         else:
-            messages.error(request,'password does not match')
+            messages.error(request, 'Passwords do not match.')
             return redirect('register') 
     else:
-        return render(request,'accounts/register.html')
+        return render(request, 'accounts/register.html')
+
 
 @login_required(login_url = 'login')
 def dashboard(request):
